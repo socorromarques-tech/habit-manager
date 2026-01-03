f3d5f03795122f7adb6f64a3d8bf6ad4cf3532cd
socorromarques <msocorrosmarques@gmail.com>
2026-01-03 20:11:36 +0000

    Fix build: Downgrade to Prisma 5 and revert client config

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -6,11 +6,6 @@ export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
     log: ['query'],
-    datasources: {
-      db: {
-        url: process.env.DATABASE_URL as string,
-      },
-    },
   });
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
